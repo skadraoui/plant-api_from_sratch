699b47f06702ea4be5b2752beec8c1add1c964fd
khadraoui <khadraoui.saif@gmail.com>
2021-01-06 11:40:56 +0100

    init plan api from sratch

---
+++ src/main/java/com/saif/symphonie/plantapi/PlantApiApplication.java
@@ -0,0 +1,13 @@
+package com.saif.symphonie.plantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saif/symphonie/plantapi/PlantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saif.symphonie.plantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
